fd683c54834e487a44ae62809efc4dce549ba186
Sergey <sergey.riedel@gmail.com>
2020-10-02 10:07:05 +0200

    Improve error message

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorMessage = ({ text }) => {
   return (
     <div className="text-center border-none py-6">
-      <h2 className="font-semibold text-gray-800">{text}</h2>
+      <h2 className="font-semibold text-gray-800 text-xl">{text}</h2>
     </div>
   );
 };
