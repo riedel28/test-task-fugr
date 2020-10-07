ecb02cedba47fa7da261648fd99b669a149425b8
Sergey <sergey.riedel@gmail.com>
2020-10-07 10:52:15 +0200

    Add tests for the App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 
-test('renders App component', () => {
-  const { getByText } = render(<App />);
-  const button = getByText(/Добавить в таблицу/i);
+describe('App', () => {
+  test('renders App component', () => {
+    const { getByText } = render(<App />);
+    const button = getByText(/Добавить в таблицу/i);
 
-  expect(button).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  test('should show preloader', () => {
+    const { getByTestId } = render(<App />);
+    const preloader = getByTestId('preloader');
+
+    expect(preloader).toBeInTheDocument();
+  });
+
+  test('should show pagination', async () => {
+    const { queryByTestId, getByText, findByTestId } = render(<App />);
+
+    expect(queryByTestId('pagination')).toBeNull();
+
+    fireEvent.click(getByText(/Показать много/i));
+
+    expect(await findByTestId('pagination')).toBeInTheDocument();
+  });
 });
