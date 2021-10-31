85b92c544bef84e843d5e7e881e7b647ffbb9298
Sergey Riedel <sergio.280686@gmail.com>
2021-10-31 10:17:45 +0100

    Set url depending on environment

--- src/api.js
+++ src/api.js
@@ -1,5 +1,12 @@
 export const getUrl = (amount) => {
-  const baseUrl = `http://www.filltext.com/?rows=${amount}&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
+  let url = '';
+  if (process.env.NODE_ENV === 'development') {
+    url = 'http://www.filltext.com';
+  } else {
+    url = 'https://www.filltext.com';
+  }
+
+  const baseUrl = `${url}/?rows=${amount}&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
 
   return baseUrl;
 };
